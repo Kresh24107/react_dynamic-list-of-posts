fc8c768e5e24abdc528f36f8d4210ba29f93d6da
Vadim Sirenko <vitalievichkresh@gmail.com>
2019-06-11 20:56:44 +0300

    Updates

---
+++ precache-manifest.d5904603c2402e9aec9a4ed8599b5d8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "575c79cb64d6e6cf4271744b0f6471a8",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "87dc39a5e37a6a0a7c05",
+    "url": "/react_dynamic-list-of-posts/static/css/main.08163736.chunk.css"
+  },
+  {
+    "revision": "bca943ab24bdfcd15706",
+    "url": "/react_dynamic-list-of-posts/static/js/2.f0927397.chunk.js"
+  },
+  {
+    "revision": "87dc39a5e37a6a0a7c05",
+    "url": "/react_dynamic-list-of-posts/static/js/main.723b5abb.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
